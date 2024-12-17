2882abfefe5777e35b15967dc0f79a158f668ea2
Netanel Segal <segal.netanel4@gmail.com>
2024-12-17 21:58:17 +0200

    fixed cors config

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,12 @@ import { Logger } from './utils/Logger';
 const app = express();
 
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
