2df4b5b69906194b4985128f22b331041ac604a7
Netanel Segal <segal.netanel4@gmail.com>
2024-12-29 15:34:59 +0200

    added socket connetion with logs

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,13 @@ import cookieParser from 'cookie-parser';
 import './db/mongo-connect';
 import { indexRouter } from './routes/index.route';
 import { Logger } from './utils/Logger';
+import { createSocket, socketInit } from './services/socket.service';
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// middlewares
 app.use(cookieParser());
 app.use(
   cors({
@@ -18,10 +22,13 @@ app.use(
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+// routes
 app.use(indexRouter);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   Logger.success(`server is up on: http://localhost:${PORT}`);
 });
+
+// socket
+const io = createSocket(server);
+socketInit(io);
