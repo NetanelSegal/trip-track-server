544b6556d016689cf9205a3d6af645571efbe0ab
Netanel Segal <segal.netanel4@gmail.com>
2025-01-06 21:08:32 +0200

    fixed redis time

--- src/validationSchemas/authSchemas.ts
+++ src/validationSchemas/authSchemas.ts
@@ -2,15 +2,27 @@ import { z } from 'zod';
 
 export const sendCodeSchema = z
   .object({
-    email: z.string().email(),
+    email: z
+      .string()
+      .email({ message: 'Please provide a valid email address.' }),
   })
   .strict();
 
 export const verifyCodeSchema = z
   .object({
-    email: z.string().email(),
-    code: z.string().min(6).max(6),
-    name: z.string().min(2).max(30),  
+    code: z
+      .string()
+      .min(6, { message: 'Code must be exactly 6 characters' })
+      .max(6, { message: 'Code must be exactly 6 characters' }),
+
+    name: z
+      .string()
+      .min(2, { message: 'Name must be at least 2 characters long' })
+      .max(15, { message: 'Name must be at most 15 characters long' }),
+
+    email: z
+      .string()
+      .email({ message: 'Please provide a valid email address.' }),
   })
   .strict();
 
