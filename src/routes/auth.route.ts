6c81126596bff5087790f196cd0130215609c892
NetanelSegal <segal.netanel4@gmail.com>
2025-02-14 14:57:59 +0200

    added what needed for user name update

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,27 +1,21 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
-  sendCode,
-  verifyCode,
-  validateToken,
-  createGuestToken,
-} from "../controllers/auth.controller";
-import { validateRequest } from "../middlewares/validatorRequest";
-import { authenticateToken } from "../middlewares/authenticateToken";
-import { Schemas } from "trip-track-package";
+	sendCode,
+	verifyCode,
+	validateToken,
+	createGuestToken,
+	generateUserTokens,
+} from '../controllers/auth.controller';
+import { validateRequest } from '../middlewares/validatorRequest';
+import { authenticateToken } from '../middlewares/authenticateToken';
+import { Schemas } from 'trip-track-package';
 
 const router = Router();
 
-router.post(
-  "/send-code",
-  validateRequest(Schemas.auth.sendCodeSchema),
-  sendCode
-);
-router.post(
-  "/verify-code",
-  validateRequest(Schemas.auth.verifyCodeSchema),
-  verifyCode
-);
-router.get("/create-guest-token", createGuestToken);
-router.get("/validate-token", authenticateToken, validateToken);
+router.post('/send-code', validateRequest(Schemas.auth.sendCodeSchema), sendCode);
+router.post('/verify-code', validateRequest(Schemas.auth.verifyCodeSchema), verifyCode);
+router.get('/create-user-tokens', authenticateToken, generateUserTokens);
+router.get('/create-guest-token', createGuestToken);
+router.get('/validate-token', authenticateToken, validateToken);
 
 export { router as authRouter };
