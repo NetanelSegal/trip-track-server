96a90b25b5965373eed7de2a48828d3fe5e130c9
Netanel Segal <segal.netanel4@gmail.com>
2025-03-16 19:11:37 +0200

    delete __v from trip

--- src/models/trip.model.ts
+++ src/models/trip.model.ts
@@ -1,52 +1,50 @@
 import mongoose, { Schema, Types } from 'mongoose';
-import {
-  ExperienceType,
-  Types as TTTypes,
-  TripStatusArray,
-} from 'trip-track-package';
+import { ExperienceType, Types as TTTypes, TripStatusArray } from 'trip-track-package';
 
 const StopSchema = new Schema<TTTypes['Trip']['Stop']['Model']>(
-  {
-    location: {
-      lon: { type: Number, required: true },
-      lat: { type: Number, required: true },
-    },
-    address: String,
-    experience: {
-      type: {
-        type: String,
-        enum: Object.values(ExperienceType),
-      },
-      data: Schema.Types.Mixed,
-      score: Number,
-    },
-  },
-  {
-    _id: false,
-  }
+	{
+		location: {
+			lon: { type: Number, required: true },
+			lat: { type: Number, required: true },
+		},
+		address: String,
+		experience: {
+			type: {
+				type: String,
+				enum: Object.values(ExperienceType),
+			},
+			data: Schema.Types.Mixed,
+			score: Number,
+		},
+	},
+	{
+		_id: false,
+	}
 );
 
 const RewardSchema = new Schema<TTTypes['Trip']['Reward']['Model']>(
-  {
-    image: String,
-    title: { type: String, required: true },
-  },
-  {
-    _id: false,
-  }
+	{
+		image: String,
+		title: { type: String, required: true },
+	},
+	{
+		_id: false,
+	}
 );
 
-const TripSchema = new Schema<TTTypes['Trip']['Model']>({
-  creator: Types.ObjectId,
-  guides: [Types.ObjectId],
-  name: String,
-  description: String,
-  stops: [StopSchema],
-  reward: RewardSchema,
-  status: { type: String, enum: TripStatusArray, default: 'created' },
-});
-
-export const Trip = mongoose.model<TTTypes['Trip']['Model']>(
-  'Trip',
-  TripSchema
+const TripSchema = new Schema<TTTypes['Trip']['Model']>(
+	{
+		creator: Types.ObjectId,
+		guides: [Types.ObjectId],
+		name: String,
+		description: String,
+		stops: [StopSchema],
+		reward: RewardSchema,
+		status: { type: String, enum: TripStatusArray, default: 'created' },
+	},
+	{
+		versionKey: false,
+	}
 );
+
+export const Trip = mongoose.model<TTTypes['Trip']['Model']>('Trip', TripSchema);
