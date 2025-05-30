7da39a412bcace87469429827b1fa463a44466a4
Netanel <segal.netanel4@gmail.com>
2025-05-30 17:00:32 +0300

    finished

--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -44,10 +44,10 @@ export const socketInit = (io: SocketServer): void => {
 		socketEventValidator(socket);
 
 		socketDataValidator(socket, 'joinTrip', socketDataSchema.joinTrip);
-		socket.on('joinTrip', (tripId) => {
+		socket.on('joinTrip', async (tripId, userId) => {
 			socket.join(tripId);
-			socket.to(tripId).emit('tripJoined', socket.id);
-
+			const user = await redisGetUserTripData(tripId, userId);
+			socket.to(tripId).emit('tripJoined', { userId, ...user });
 			Logger.info(`User ${socket.id} joined trip room: ${tripId}`);
 		});
 
